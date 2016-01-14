fec9688ae311829ab8e52fc16b8a7855e020fbac
Дмитрий Седых <dmitrys@xyzrd.com>
2016-01-14 17:25:05 +0300

    format

--- password.go
+++ password.go
@@ -2,7 +2,8 @@ package model
 
 import "golang.org/x/crypto/bcrypt"
 
-// Password описывает тип для пароля, хранящегося в виде хеш с использованием алгоритма bcrypt.
+// Password описывает тип для пароля, хранящегося в виде хеш с использованием
+// алгоритма bcrypt.
 type Password []byte
 
 // NewPassword возвращает пароль в виде хеш.
@@ -14,8 +15,9 @@ func NewPassword(password string) Password {
 	return Password(passwd)
 }
 
-// Compare сравнивает сохраненный в виде хеш пароль с указанным в параметре и возвращает true,
-// если указанный пароль с очень большой степенью вероятности и является оригинальным паролем.
+// Compare сравнивает сохраненный в виде хеш пароль с указанным в параметре и
+// возвращает true, если указанный пароль с очень большой степенью вероятности и
+// является оригинальным паролем.
 func (p Password) Compare(password string) bool {
 	return bcrypt.CompareHashAndPassword(p, []byte(password)) == nil
 }
